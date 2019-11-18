0429b82a99947ca08139304ffc4df3e8d5614ed6
Elias Vargas <elias.vargas@globant.com>
2019-11-18 14:43:59 -0300

    Finish Collectors 6.3

--- src/test/java/com/cl/java/test/example/streams/CollectingStreamsTest.java
+++ src/test/java/com/cl/java/test/example/streams/CollectingStreamsTest.java
@@ -7,12 +7,19 @@ import org.junit.runners.JUnit4;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.averagingInt;
 import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.reducing;
+import static java.util.stream.Collectors.summarizingInt;
+import static java.util.stream.Collectors.summingInt;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(JUnit4.class)
 public class CollectingStreamsTest {
@@ -38,6 +45,40 @@ public class CollectingStreamsTest {
     public void testMaxValueCollector() {
         Optional<Dish> maxCaloriesDish = menu.stream().collect(maxBy(Comparator.comparing(Dish::getCalories)));
         assertEquals(800, maxCaloriesDish.get().getCalories());
+    }
+
+    @Test
+    public void testSumCollector(){
+        int totalCalories = menu.stream().collect(summingInt(Dish::getCalories));
+        assertEquals(4200, totalCalories);
+    }
+
+    @Test
+    public void testAverageCollector(){
+        Double avgCalories = menu.stream().collect(averagingInt(Dish::getCalories));
+        assertTrue(avgCalories > 400);
+    }
+
+    @Test
+    public void testStaticsCollector(){
+        IntSummaryStatistics menuStatics = menu.stream().collect(summarizingInt(Dish::getCalories));
+        assertEquals(4200, menuStatics.getSum());
+        assertEquals(800, menuStatics.getMax());
+        assertEquals(9, menuStatics.getCount());
+    }
+
+    @Test
+    public void testJoinStringDelimiterCollector(){
+        String delimiter = ";";
+        String menuNames = menu.stream().map(Dish::getName).collect(Collectors.joining(delimiter));
+        long count = Arrays.asList(menuNames.split(delimiter)).stream().count();
+        assertEquals(menu.stream().count(), count);
+    }
+
+    @Test
+    public void testReducingCollector(){
+        int totalCalories = menu.stream().collect(reducing(0, Dish::getCalories, (i, j) -> i+j));
+        assertEquals(4200, totalCalories);
     }
 
 }
